7355d2f1d8ca1c2b0e2a0431484a7fcae76f8b75
Parbjot Bhullar <parbjotb@gmail.com>
2021-07-11 12:33:24 -0500

    update main.js global filters
    commented out global filters to use them locally instead

--- src/main.js
+++ src/main.js
@@ -25,16 +25,16 @@ Vue.filter('snippet', function(value){
 // we have v-rainbow in showBlogs.vue so param name is rainbow
 // 2nd param is an object which controls functionality of the directive
 // bind is a lifestyle hook, as soon as a connection is made, the bind method fires
-Vue.directive('rainbow', {
-  bind(el, binding, vnode){
-    // el refers to element itself, so in showBlogs it was h2
-    // binding refers to data, so if we did v-rainbow="something"
-    // vnode is virtual nodes
-    // this gives the h2 element a random color 6 digit hash code
-    // the code just gives us a 6 digit number and converts to a string
-    el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
-});
+// Vue.directive('rainbow', {
+//   bind(el, binding, vnode){
+//     // el refers to element itself, so in showBlogs it was h2
+//     // binding refers to data, so if we did v-rainbow="something"
+//     // vnode is virtual nodes
+//     // this gives the h2 element a random color 6 digit hash code
+//     // the code just gives us a 6 digit number and converts to a string
+//     el.style.color = "#" + Math.random().toString().slice(2,8);
+//   }
+// });
 
 // this will make the element in showBlogs.vue have a wide max maxWidth
 // follows same protocol as above directive
